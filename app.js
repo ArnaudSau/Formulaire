a9d97970f43db91dd22f346a693ea62991c3f2de
ArnaudSau <arnaud.saudrais@yahoo.com>
2020-07-10 15:36:05 +0200

    formv2

--- app.js
+++ app.js
@@ -1,26 +1,104 @@
+//ASYNCRONE
 document.getElementById("inscription").addEventListener("submit", function(e) {
-    e.preventDefault();
+    e.preventDefault(); // comportement par defaut désactivé
 
-    var data = new FormData(this);
-    var xhr = new XMLHttpRequest();
+    if(verifForm(this)){ //Vérification du formulaire
 
-    xhr.onreadystatechange = function() {
-      if(this.readyState == 4 && this.status == 200){
-        console.log(this.response);
-        var res = this.response;
-        if (res.success){
-          console.log("Utilisateur inscrit !");
-        } else {
-          alert(this.response.msg);
+      var data = new FormData(this);
+      var xhr = new XMLHttpRequest();
+
+      xhr.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){  // Vérification de la requete
+          console.log(this.response);
+          var res = this.response;
+          if (res.success){
+            alert(this.response.msg);
+            console.log("Utilisateur inscrit !"); // OK
+          } else {
+            alert(this.response.msg); //message d'erreur provenant du PHP
+          }
+        }else if (this.readyState == 4){  // Erreur lors de la requete
+          alert("Une erreur est survenue !");
         }
-      }else if (this.readyState == 4){
-        alert("Une erreur est survenue !");
       }
-    }
 
-    xhr.open("POST","async/script.php",true);
-    xhr.responseType = "json"
-    xhr.send(data);
+      xhr.open("POST","async/script.php",true);  // echange avec script.php en POST
+      xhr.responseType = "json"   // force le type de response en json
+      xhr.send(data); // envoie les données du formulaire
 
-    return false;
+      return false;
+  }
 });
+
+//SURLIGNE
+function surligne(champ, erreur) // Surligne le champ en rouge si erreur
+{
+   if(erreur)
+      champ.style.backgroundColor = "#fba";
+   else
+      champ.style.backgroundColor = "";
+}
+
+//***************
+// VERIFICATION DES CHAMPS
+//***************
+function verifNom(champ) // Vérification du champs Nom
+{
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true); //erreur -> surligne en rouge
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifMail(champ) // Vérification du champ mail
+{
+   var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+   if(!regex.test(champ.value))
+   {
+      surligne(champ, true);  //erreur -> surligne en rouge
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifPortable(champ)  // Vérification du champ portable
+{
+  var regex = new RegExp(/^(06|07)[0-9]{8}/gi);
+
+  if(!regex.test(champ.value))
+  {
+     surligne(champ, true); //erreur -> surligne en rouge
+     return false;
+  }
+  else
+  {
+     surligne(champ, false);
+     return true;
+  }
+}
+
+function verifForm(f)   //Vérification de tous les champs lors du submit
+{
+   var nomOk = verifNom(f.nom);
+   var prenomOk = verifNom(f.prenom);
+//   var mailOk = verifMail(f.email);  Vérif fait par le php
+   var portableOk = verifPortable(f.telephone);
+
+   if(nomOk && prenomOk && portableOk)
+      return true;
+   else
+   {
+      alert("Veuillez remplir correctement tous les champs");
+      return false;
+   }
+}
--- async/script.php
+++ async/script.php
@@ -1,25 +1,30 @@
 <?php
-//  echo json_encode($_POST);
+
+  //Variables pour la requete
   $success = 0;
   $msg = "Une erreur est survenue !";
+
+  //Variables pour la bdd
   $serveur = "localhost";
   $login = "root";
   $pass = "";
   $bdd = new PDO("mysql:host=$serveur;dbname=inscrit",$login,$pass);
 
-  if (!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['email']) AND !empty($_POST['telephone'])){
+  if (!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['email']) AND !empty($_POST['telephone'])){ //Vérifie que ce soit non vide
+
+    //Attributions des valeur des champs auc variables
     $nom = htmlspecialchars(strip_tags($_POST['nom']));
     $prenom = htmlspecialchars(strip_tags($_POST['prenom']));
     $email = htmlspecialchars(strip_tags($_POST['email']));
     $telephone = htmlspecialchars(strip_tags($_POST['telephone']));
 
-    if(filter_var($email,FILTER_VALIDATE_EMAIL)){
+    if(filter_var($email,FILTER_VALIDATE_EMAIL)){ // Vérifie l'email
 
-      // Ajout bdd
-      $requete = $bdd->prepare("INSERT INTO inscrit(nom, prenom, email, telephone) VALUES(?, ?, ?, ?)");
-      $requete->execute(array($nom, $prenom, $email, $telephone));
+      // AJOUT BDD
+      $requete = $bdd->prepare("INSERT INTO inscrit(nom, prenom, email, telephone) VALUES(?, ?, ?, ?)"); //requete préparé
+      $requete->execute(array($nom, $prenom, $email, $telephone));   // execution
       $success = 1;
-      $msg = "Succes";
+      $msg = "Vous avez bien été inscrit à l'événement";
 
     } else {
       $msg = "Adresse email invalide";
@@ -30,6 +35,6 @@
   }
 
   $res = ["success" => $success,"msg" => $msg];
-  echo json_encode($res);
+  echo json_encode($res); // Valeur echangé avec le Javascript
 
  ?>
